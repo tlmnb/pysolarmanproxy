070489935f6c4c4ae808ffddaea25ba6c4eb294a
Tilman Berger <1886755+tlmnb@users.noreply.github.com>
2025-04-03 21:40:12 +0200

    added __version__

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
+from pysolarmanproxy import __version__
 
 setup(
     name="pysolarmanproxy",
-    version="1.0.0",
+    version=__version__,
     description="minimal proxy server which exposes a solarman device by rtu-over-tcp modbus",
     packages=find_packages(),
     install_requires=[
